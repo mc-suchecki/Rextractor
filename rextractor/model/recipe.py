5b00343f499734f6fc824d1f2237175c9e1fa53f
siekiera <mistrzsiekiera@gmail.com>
2014-12-12 00:05:16 +0100

    Stub of NLProcessor and model classes

--- rextractor/model/recipe.py
+++ rextractor/model/recipe.py
@@ -1,1 +1,19 @@
 # TODO
+
+
+class RawRecipe:
+    """ Represents a recipe containing raw HTML data
+    """
+    pass
+
+
+class ParsedRecipe:
+    """ A recipe with text representation of ingredients
+    """
+    pass
+
+
+class ProcessedRecipe:
+    """ Object representation of a recipe
+    """
+    pass
---
+++ rextractor/nlprocessor/nlprocessor.py
@@ -0,0 +1,15 @@
+__author__ = 'Michał Toporowski'
+# TODO
+
+
+class NLProcessor:
+    """ The natural language processing module
+    """
+
+    def process(self, parsed_recipes):
+        """ Processes the ParsedRecipe containing text information to ProcessedRecipe containing object information
+
+        :param parsed_recipes: list of ParsedRecipe objects
+        :return: list of ProcessedRecipe objects
+        """
+        pass
